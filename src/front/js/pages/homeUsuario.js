94bf1a9571c694cf601619608f695a75566ae09a
Pamela Contreras <pame2394@gmail.com>
2021-05-01 05:10:11 +0000

    home usuario

--- src/front/js/pages/homeUsuario.js
+++ src/front/js/pages/homeUsuario.js
@@ -9,13 +9,18 @@ export const HomeUsuario = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
-			<br />
-			<p>HOME Usuario</p>
-
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
+		<div className="container ">
+			<h1 style={{ paddingTop: "0.7cm" }}>Limón</h1>
+			<div className="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2 ">
+				<div>{/* <People name={element.name} url={peoplePics[element.uid]} /> */}</div>
+			</div>
+			<h1 style={{ paddingTop: "0.7cm" }}>San José</h1>
+			<div className="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
+				<div>{/* <Planets /> */}</div>
+			</div>
+			<a className="btn btn-primary" href="/" role="button">
+				Back home
+			</a>
 		</div>
 	);
 };
