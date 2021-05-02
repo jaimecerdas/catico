c24764e4c14bb5503683e7d42a888a39a5f8a027
J Cerdas <jaimecerdas@gmail.com>
2021-05-02 02:55:04 +0000

    v1

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { Context } from "../store/appContext";
+import footerImage from "/workspace/Proyecto_final_2021/src/front/img/footer.gif";
+import "../../styles/footer.scss";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">CampingTico</a>
+			<i className="fab fa-facebook-square" />
+			<a href="http://www.facebook.com">/CampingTico </a>
+			<i className="fab fa-instagram" /> <a href="http://www.instagram.com">/CampingTico </a>{" "}
 		</p>
+		<p>CampingTico © 2021</p>
 	</footer>
 );
--- src/front/js/pages/homeEmpresario.js
+++ src/front/js/pages/homeEmpresario.js
@@ -11,15 +11,72 @@ export const HomeEmpresario = () => {
 	return (
 		<div className="container">
 			<br />
-			<p>HOME EMPRESARIO</p>
+			<div className="jumbotron">
+				<h1 className="display-4">¡Hola, (Nombre)!</h1>
+				<p className="lead">Gracias por utilizar CampingTico.</p>
+				<hr className="my-4" />
+				<p>Ingresa un nuevo lugar:</p>
+				<p className="lead">
+					<a className="btn btn-primary btn-lg" href="#" role="button">
+						Agregar un lugar
+					</a>
+				</p>
+			</div>
 
-			<p>MIS LUGARES</p>
-			<p>Aqui van mis lugares</p>
-			<p>CREAR UN NUEVO LUGAR</p>
-			<p>Aqui va el form</p>
+			<div className="cards">
+				<h1 className="display-4">Tus Lugares</h1>
+				<div className="card">
+					<div className="card-body">
+						<h5 className="card-title">Nombre del Lugar</h5>
+						<p className="card-text">Descripcion del lugar</p>
+						<img
+							className="card-img-top my-3"
+							src="https://www.intermundial.es/blog/wp-content/uploads/2014/06/shutterstock_64920532.jpg"
+							alt="Card image cap"
+						/>
+						<a href="#" className="btn btn-primary">
+							Detalles
+						</a>
+					</div>
+				</div>
 
+				<div className="crearLugar">
+					<h1 className="display-4">Agregar un lugar</h1>
+
+					<form>
+						<div className="form-group">
+							<label htmlFor="exampleFormControlTextarea2">Nombre</label>
+							<input className="form-control" id="exampleFormControlTextarea2" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="exampleFormControlSelect1">Provincia</label>
+							<select className="form-control" id="exampleFormControlSelect1">
+								<option>Alajuela</option>
+								<option>Cartago</option>
+								<option>Heredia</option>
+								<option>Limón</option>
+								<option>Guanacaste</option>
+								<option>San José</option>
+								<option>Puntarenas</option>
+							</select>
+						</div>
+
+						<div className="form-group">
+							<label htmlFor="exampleFormControlTextarea1">Descripción</label>
+							<textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="exampleFormControlSelect2">Ambiente</label>
+							<select className="form-control" id="exampleFormControlSelect2">
+								<option>Familiar</option>
+								<option>Adultos</option>
+							</select>
+						</div>
+					</form>
+				</div>
+			</div>
 			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
+				<button className="btn btn-primary my-5">Back home</button>
 			</Link>
 		</div>
 	);
